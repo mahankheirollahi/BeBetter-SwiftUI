c91ce333415b8547981fb04e18f14190e53d5a47
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-12-17 04:53:10 +0300

    Initial Commit

---
+++ BeBetter-SwiftUI/BeBetter_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeBetter_SwiftUIApp.swift
+//  BeBetter-SwiftUI
+//
+//  Created by Mahan Kheirollahi on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct BeBetter_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeBetter-SwiftUITests/BeBetter_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  BeBetter_SwiftUITests.swift
+//  BeBetter-SwiftUITests
+//
+//  Created by Mahan Kheirollahi on 12/17/24.
+//
+
+import Testing
+@testable import BeBetter_SwiftUI
+
+struct BeBetter_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
